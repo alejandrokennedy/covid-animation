1f95f8e621eaf96613b5dab17d5f2963196dd44f
alejandrokennedy <alejandrokennedy@gmail.com>
2020-11-20 18:05:17 -0800

    got svg and canvas together with a top margin for tall spikes

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
 //---------------------------------------------------------
 // SETUP
-
-//---------------------------------------------------------
+// //------------------------------------------------------
 // // PAGE SETUP
 
 const body = d3.select('body')
   .style('margin', '0 auto')
 
 const container = d3.select('#container')
-  .style('width', '600px')
-  .style('height', '500px')
+  // .style('width', '100%')
+  // .style('height', '500px')
+  .style('max-width', '625px')
+  .style('position', 'relative')
   .style('margin', '0 auto')
+  .style('border', '2px solid orangered')
+
+const containerBounds = container.node().getBoundingClientRect()
+const containerWidth = containerBounds.width
+
+// //------------------------------------------------------
+// // MAP SETUP
+
+const mapMargin = {top: 200, right: 0, bottom: 0, left: 0}
+
+const mapContainer = d3.select('#map-container')
   .style('border', '2px solid aqua')
 
-const bounds = container.node().getBoundingClientRect()
+const mapBounds = mapContainer.node().getBoundingClientRect()
+const mapWidth = mapBounds.width
+const mapHeight = (mapWidth / 1.6) + mapMargin.top
 
-//---------------------------------------------------------
-// // GRAPHICAL ELEMENTS SETUP
+// //------------------------------------------------------
+// // CANVAS SETUP
 
-const mapWidth = bounds.width
-const mapHeight = mapWidth / 1.6
+const dpi = window.devicePixelRatio
 
-const mapSvg = container.append('svg').attr('class', 'mapSvg')
+const mapCanvas = mapContainer.append('canvas').attr('class', 'mapCanvas')
+  .style('position', 'absolute')
+  .style("width", `${mapWidth}px`)
+  .style("height", `${mapHeight}px`)
+  .attr("width", `${mapWidth * dpi}`)
+  .attr("height", `${mapHeight * dpi}`)
+
+// //------------------------------------------------------
+// // SVG SETUP
+
+const mapSvg = mapContainer.append('svg').attr('class', 'mapSvg')
   .attr('viewBox', `0 0 ${mapWidth}, ${mapHeight}`)
 
-//---------------------------------------------------------
+// //------------------------------------------------------
 // // MISCELLANEOUS VARIABLES SETUP
 
 // ...
@@ -32,19 +55,24 @@ const mapSvg = container.append('svg').attr('class', 'mapSvg')
 //---------------------------------------------------------
 // DATA
 
-//---------------------------------------------------------
-// // SETUP DATA
-
 async function getData() {
   const us = await d3.json('./data/us.json')
 
-  //---------------------------------------------------------
+  // //------------------------------------------------------
   // // MAP DATA
 
   const usStates = topojson.feature(us, us.objects.states)
 
   const projection = d3.geoAlbersUsa()
-    .fitWidth(mapWidth, usStates)
+    // .fitWidth(mapWidth, usStates)
+    // .fitSize([mapWidth, mapHeight], usStates);
+    .fitExtent([[0, mapMargin.top], [mapWidth, mapHeight]], usStates)
+
+    // .fitExtent([[0, 0], [width, height]], object);
+    // .fitSize([width, height], object);
+    
+
+    // .translate([mapWidth/2, mapHeight / 2])
 
   const path = d3.geoPath().projection(projection)
 

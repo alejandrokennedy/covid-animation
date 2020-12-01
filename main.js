e8c3ca8474c98c359c92529a6f46aaf2dbf4b303
alejandrokennedy <alejandrokennedy@gmail.com>
2020-11-30 20:54:08 -0800

    added legends

--- main.js
+++ main.js
@@ -5,21 +5,16 @@
 
 const opacity = 0.7
 const avgNum = 14
-const excludedStates = ["66", "69", "72", "78"]
 const duration = 225
-const spikeWidth = 7
-
-const interpolator = d3.interpolateRgb('#F53844', '#42378F')
-const color = d3.scaleSequential(interpolator)
-  .domain([0, 150])
-  // .domain([150, 0])
-  .clamp(true)
+const excludedStates = ["66", "69", "72", "78"]
 
 // //------------------------------------------------------
 // // PAGE SETUP
 
 const body = d3.select('body')
   .style('margin', '0 auto')
+  .style('font-family', 'helvetica')
+  .style('font-size', '12px')
 
 const container = d3.select('#container')
   // .style('width', '100%')
@@ -27,23 +22,33 @@ const container = d3.select('#container')
   .style('max-width', '625px')
   .style('position', 'relative')
   .style('margin', '0 auto')
-  .style('border', '2px solid orangered')
+  // .style('border', '2px solid orangered')
 
 const containerBounds = container.node().getBoundingClientRect()
 const containerWidth = containerBounds.width
+
+const legendContainer = d3.select('#legendContainer')
+  .style('display', 'flex')
+  .style('flex-direction', 'row')
 
 // //------------------------------------------------------
 // // MAP SETUP
 
 const mapMargin = {top: 200, right: 0, bottom: 0, left: 0}
-const spikeMax = mapMargin.top + 140
 
 const mapContainer = d3.select('#map-container')
-  .style('border', '2px solid aqua')
+  // .style('border', '2px solid aqua')
 
 const mapBounds = mapContainer.node().getBoundingClientRect()
 const mapWidth = mapBounds.width
 const mapHeight = (mapWidth / 1.6) + mapMargin.top
+
+const spikeMax = mapMargin.top + 140
+const spikeWidth = mapWidth / 90
+
+// for chart
+const width = mapWidth
+const height = 900
 
 // //------------------------------------------------------
 // // CANVAS SETUP
@@ -61,10 +66,174 @@ const ctx = mapCanvas.node().getContext('2d')
 ctx.scale(dpi, dpi)
 
 // //------------------------------------------------------
-// // SVG SETUP
+// // MAP SVG SETUP
 
 const mapSvg = mapContainer.append('svg').attr('class', 'mapSvg')
   .attr('viewBox', `0 0 ${mapWidth}, ${mapHeight}`)
+
+// //------------------------------------------------------
+// // CHART SVG SETUP
+
+const chartSvg = d3.select('#chart-container').append('svg')
+  .attr('id', 'chartSvg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  // .style('border', '2px solid gold')
+
+// //------------------------------------------------------
+// // COLOR LEGEND SETUP
+
+const colorContainer = d3.select('#colorContainer')
+
+function legend({
+  color,
+  title,
+  tickSize = 6,
+  width = 320,
+  height = 44 + tickSize,
+  marginTop = 18,
+  marginRight = 0,
+  marginBottom = 16 + tickSize,
+  marginLeft = 0,
+  ticks = width / 64,
+  tickFormat,
+  tickValues
+} = {}) {
+
+  const svg = colorContainer.append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .style("overflow", "visible")
+    .style("display", "block");
+
+  let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+  let x;
+
+  // Continuous
+  if (color.interpolate) {
+    const n = Math.min(color.domain().length, color.range().length);
+
+    x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+  }
+
+  // Sequential
+  else if (color.interpolator) {
+
+    x = Object.assign(color.copy()
+        .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+        {range() { return [marginLeft, width - marginRight]; }});
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.interpolator()).toDataURL())
+      .attr('opacity', opacity);
+
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== "function") {
+        tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+      }
+    }
+  }
+
+  // Threshold
+  else if (color.invertExtent) {
+    const thresholds
+        = color.thresholds ? color.thresholds() // scaleQuantize
+        : color.quantiles ? color.quantiles() // scaleQuantile
+        : color.domain(); // scaleThreshold
+
+    const thresholdFormat
+        = tickFormat === undefined ? d => d
+        : typeof tickFormat === "string" ? d3.format(tickFormat)
+        : tickFormat;
+
+    x = d3.scaleLinear()
+        .domain([-1, color.range().length - 1])
+        .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.range())
+      .join("rect")
+        .attr("x", (d, i) => x(i - 1))
+        .attr("y", marginTop)
+        .attr("width", (d, i) => x(i) - x(i - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", d => d);
+
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+  }
+
+  // Ordinal
+  else {
+    x = d3.scaleBand()
+        .domain(color.domain())
+        .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.domain())
+      .join("rect")
+        .attr("x", x)
+        .attr("y", marginTop)
+        .attr("width", Math.max(0, x.bandwidth() - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", color);
+
+    tickAdjust = () => {};
+  }
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x)
+      .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+      .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+      .tickSize(tickSize)
+      .tickValues(tickValues))
+    .call(tickAdjust)
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+      .attr("x", marginLeft)
+      .attr("y", marginTop + marginBottom - height - 6)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text(title));
+
+  return svg.node();
+}
+
+function ramp(color, n = 256) {
+  const canvas = d3.create('canvas')
+    .attr('width', n)
+    .attr('height', 1)
+    .attr('id', 'legCanvEl')
+
+  const context = canvas.node().getContext("2d");
+  for (let i = 0; i < n; ++i) {
+    context.fillStyle = color(i / (n - 1));
+    context.fillRect(i, 0, 1, 1);
+  }
+  return canvas.node();
+}
 
 //---------------------------------------------------------
 // DATA
@@ -261,9 +430,59 @@ async function getData() {
     return max;
   }
 
+  console.log('maxDailyCasesCounties', maxDailyCasesCounties)
+
+  const maxPerHundThouCounties = getMaxPerHundThouCounties()
+  function getMaxPerHundThouCounties() {
+    let max = 0;
+    for (let date of frames) {
+      const maxOfDay = d3.max(date.counties.map(d => d[2]));
+      if (maxOfDay > max) max = maxOfDay;
+      if (maxOfDay > 250)
+        console.log(
+          // date.date,
+          // 'county: ' +
+          //   date.counties[d3.maxIndex(date.counties.map(d => d[2]))][0],
+          // 'state: ' +
+          // fipsToStateLookup.get(
+          //   date.counties[d3.maxIndex(date.counties.map(d => d[2]))][0].slice(
+          //     0,
+          //     2
+          //   )
+          // ),
+          // 'cases: ' + date.counties[d3.maxIndex(date.counties.map(d => d[2]))][1],
+          // 'pop: ' +
+          countiesPop.get(
+            date.counties[d3.maxIndex(date.counties.map(d => d[2]))][0]
+          )
+          // maxOfDay
+        );
+    }
+    return max;
+  }
+
+  console.log('maxPerHundThouCounties', maxPerHundThouCounties)
+
   const length = d3.scaleLinear()
     .domain([0, maxDailyCasesCounties])
     .range([0, spikeMax])
+
+  // const interpolator = d3.interpolateRgb('#F53844', '#42378F')
+  // const interpolator = d3.piecewise(d3.interpolateHsl, ['#0000ff', '#ff3b3b', '#ff0000'])
+  const interpolator = d3.piecewise(d3.interpolateHsl, ['#0000ff', '#c9ff87', '#700000'])
+  const color = d3.scaleSequential(interpolator)
+    .domain([0, maxPerHundThouCounties])
+    // .domain([0, 150])
+    // .domain([150, 0])
+    .clamp(true)
+
+  legend({
+    color: color,
+    title: "New Cases Per 100,000 People",
+    width: mapWidth / 1.8,
+    marginLeft: 15,
+    marginRight: 15
+  })
 
   // //------------------------------------------------------
   // // DATA: RANKING
@@ -355,8 +574,6 @@ async function getData() {
   
   d3.select('#scrubInput').attr('max', keyFrames.length - 1)
 
-  function scrub(form) { update(form.value) }
-
   // //------------------------------------------------------
   // // DRAWING: SPIKES
 
@@ -392,6 +609,231 @@ async function getData() {
     }
   }
 
-}
+  // //------------------------------------------------------
+  // // DRAWING: SPIKE LEGEND
+  
+  const makeSpike = length => `M${-spikeWidth / 2},0L0,${-length}L${spikeWidth / 2},0`
+
+  const spikeLegend = mapSvg.append('g')
+    .attr('class', 'spikeLegend')
+    .attr('text-anchor', 'middle')
+    .attr('font-size', 10)
+
+  const spikeLegendGs = spikeLegend.selectAll('g')
+    .data(length.ticks(4).slice(1).reverse())
+   .join('g')
+    .attr('transform', (d, i) => `translate(${mapWidth + 10 - (i + 1) * 15},${mapHeight - 13})`)
+
+  spikeLegendGs.append('path')
+    .style('opacity', opacity)
+    .attr('d', d => makeSpike(length(d)))
+
+  spikeLegendGs.append('text')
+    .attr('dy', '1.1em')
+    .text(length.tickFormat(4, "s"))
+
+  spikeLegend.append('text')
+    .attr('dy', '1.1em')
+    .attr('text-anchor', 'end')
+    .attr("font-weight", "bold")
+    .attr('transform', `translate(${mapWidth - 75},${mapHeight - 13})`)
+    .text('New Cases')
+
+  //------------------------------------------------------
+  // BARS
+  // //------------------------------------------------------
+  // // BARS: FUNCTIONS
+
+  const margin = { top: 10, right: 50, bottom: 6, left: 170 }
+
+  const x = d3.scaleLinear()
+    .domain([0, 1])
+    .range([margin.left, width - margin.right])
+
+  const y = d3.scaleBand()
+    .domain(d3.range(states.length))
+    .rangeRound([margin.top, height - margin.top - margin.bottom])
+    .padding(0.1)
+
+  const xAxis = d3
+    .axisTop(x)
+    .tickSizeOuter(0)
+    .tickSizeInner(-height + margin.top + margin.bottom)
+
+  const axis = svg => {
+    const g = svg.append('g').attr('transform', `translate(0,${margin.top})`);
+  
+    return (_, transition, largestBarVal) => {
+      const min = Math.floor(largestBarVal);
+      const max = width / 160;
+      xAxis.ticks(min > max ? max : min);
+  
+      g.transition(transition).call(xAxis);
+      // g.select('.tick:first-of-type text').remove();
+      g.selectAll('.tick line').attr('stroke', 'white');
+      g.select('.domain').remove();
+    };
+  }
+
+  const labels = svg => {
+    let label = svg
+      .append('g')
+      .style("font", "bold 12px var(--sans-serif)")
+      .style("font-variant-numeric", "tabular-nums")
+      .attr('text-anchor', 'end')
+      .selectAll('text');
+  
+    return (data, transition) =>
+      (label = label
+        .data(data.statesRanked, d => d.state)
+        .join(
+          enter =>
+            enter
+              .append('text')
+              // change
+              .attr('transform', d => `translate(${(x(0), y(d.rank))})`)
+              .attr('y', y.bandwidth() / 2)
+              .attr('x', -4)
+              .attr('dy', '0.25em')
+              // .attr('text-anchor', 'end')
+              .style('opacity', 0)
+              .text(d => d.state),
+          update => update,
+          exit => exit.transition(transition).remove()
+        )
+        .call(label =>
+          label
+            .transition(transition)
+            .attr(
+              'transform',
+              d =>
+                `translate(${d.value ? x(d.value.smaRound) : x(0)}, ${y(d.rank)})`
+            )
+            .style('opacity', d => (d.value.smaRound === 0 ? 0 : 1))
+        ));
+  }
+
+  const values = svg => {
+    let value = svg
+      .append('g')
+      .style("font", "12px var(--sans-serif)")
+      .style("font-variant-numeric", "tabular-nums")
+      .attr('text-anchor', 'start')
+      .selectAll('text');
+  
+    return (data, transition) =>
+      (value = value
+        .data(data.statesRanked, d => d.state)
+        .join(
+          enter =>
+            enter
+              .append('text')
+              // change
+              .attr('transform', d => `translate(${(x(0), y(d.rank))})`)
+              .attr('y', y.bandwidth() / 2)
+              .attr('x', 3)
+              .attr('dy', '0.25em')
+              .style('opacity', 0)
+              .text(d => (d.value ? d.value.smaRound : 0)),
+          update => update,
+          exit => exit.transition(transition).remove()
+        )
+        .call(value => {
+          return value
+            .transition(transition)
+            .attr(
+              'transform',
+              d =>
+                `translate(${d.value ? x(d.value.smaRound) : x(0)}, ${y(d.rank)})`
+            )
+            .style('opacity', d => (d.value.smaRound === 0 ? 0 : 1))
+            .tween('text', d => {
+              if (!prev.get(d) && d.value) return textTween(0, d.value.smaRound);
+              return prev.get(d) && d.value
+                ? !prev.get(d).value
+                  ? textTween(0, d.value.smaRound)
+                  : textTween(prev.get(d).value.smaRound, d.value.smaRound)
+                : textTween(0, 0);
+            });
+        }));
+  }
+
+  function textTween(a, b) {
+    const i = d3.interpolateNumber(a, b);
+    return function(t) {
+      this.textContent = formatNumber(i(t));
+    };
+  }
+
+  const formatNumber = d3.format(",d")
+
+  const bars = svg => {
+    let bar = svg
+      .append('g')
+      .attr('fill-opacity', 0.4)
+      .selectAll('rect');
+  
+    return (data, transition) => {
+      return (bar = bar
+        .data(data.statesRanked, d => d.state)
+        .join(
+          enter =>
+            enter
+              .append("rect")
+              // .attr("fill", color)
+              .attr("height", y.bandwidth())
+              .attr("x", x(0))
+              // .attr("y", d => y((prev.get(d) || d).rank))
+              .attr("y", d => y.range()[1])
+              .attr("width", d => x((prev.get(d) || d).value.smaRound) - x(0)),
+          update => update,
+          exit =>
+            exit
+              .transition(transition)
+              .remove()
+              // .attr("y", d => y((next.get(d) || d).rank))
+              .attr("y", d => y(d.rank))
+              // .attr("width", d => x((next.get(d) || d).value) - x(0))
+              .attr("width", d => x(d.value - x(0)))
+        )
+        .call(bar =>
+          bar
+            .transition(transition)
+            .attr("y", d => y(d.rank))
+            .attr("width", d => (d.value ? x(d.value.smaRound) - x(0) : 0))
+            .attr('fill', d => color(d.value.perHundThou))
+        ));
+    };
+  }
+
+  // //------------------------------------------------------
+  // // BARS: DRAWING THINGS
+
+  const updateBars = bars(chartSvg);
+  const updateAxis = axis(chartSvg);
+  const updateLabels = labels(chartSvg);
+  const updateValues = values(chartSvg);
+  // const updateTicker = ticker(chartSvg);
+
+  function scrub(form) {
+    const keyframe = form.value
+
+    const transition = chartSvg.transition()
+      .duration(duration)
+      .ease(d3.easeLinear)
+
+    const largestBarVal = d3.max([keyframe.statesRanked[0].value.smaRound, 1]);
+    x.domain([0, largestBarVal]);
+
+
+    updateAxis(keyframe, transition, largestBarVal);
+    updateBars(keyframe, transition);
+    updateLabels(keyframe, transition);
+    updateValues(keyframe, transition);
+    // updateTicker(keyframe, transition);
+
+    update(keyframe)
+  }
+} // getData callback
 
 getData()
